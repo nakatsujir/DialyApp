876aa9c3021e60cd4bbb192cbf962b2c74c7bb1d
nakatsuji <rie.nakatsuji1123@gmail.com>
2019-07-09 10:30:14 +0900

    Preview画面text更新

--- app/src/main/java/jp/rie/ijichi/dialyapp/PreviewActivity.kt
+++ app/src/main/java/jp/rie/ijichi/dialyapp/PreviewActivity.kt
@@ -5,17 +5,36 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabaseReference: DatabaseReference
+
+    private lateinit var mDialy:Diary
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
 
-        val titleText = intent.getStringExtra(MainActivity.TITLE_TEXT)
-        preview_title_edit.text = titleText
+        mAuth = FirebaseAuth.getInstance()
+
+        val extras = intent.extras
+        mDialy = extras.get("diary") as Diary
+
+        preview_day_text.text = mDialy.day
+        preview_title_edit.text = mDialy.title
+        preview_text_edit.text = mDialy.text
+
+
+
+
 
     }
 
